e16ffc2254e0b995f210bcf565aa3aa3ba0b45f9
lijie <crazyjie262@gmail.com>
2020-06-10 17:25:23 +0800

    add SwaggerConfig

--- src/main/java/com/jack/admin/config/SwaggerConfig.java
+++ src/main/java/com/jack/admin/config/SwaggerConfig.java
@@ -2,7 +2,6 @@ package com.jack.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.util.UriComponentsBuilder;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -12,7 +11,6 @@ import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
-import springfox.documentation.spring.web.paths.AbstractPathProvider;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
