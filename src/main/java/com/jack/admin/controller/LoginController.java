00752661ea6c0e875eedd4fa421b8007b8581483
lijie <541143520@qq.com>
2020-06-10 22:44:31 +0800

    update

--- src/main/java/com/jack/admin/controller/LoginController.java
+++ src/main/java/com/jack/admin/controller/LoginController.java
@@ -4,6 +4,10 @@ import com.jack.admin.entity.vo.SystemUserVo;
 import com.jack.admin.service.SystemUserService;
 import com.jack.admin.util.JwtUtil;
 import com.jack.admin.util.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
@@ -21,17 +25,19 @@ import javax.servlet.ServletRequest;
 @RestController
 @RequestMapping("user")
 @AllArgsConstructor
-@ApiIgnore
+@Api(description = "登录控制器")
 public class LoginController {
 
     private final SystemUserService systemUserService;
 
-    @PostMapping("/login")
+    @ApiOperation(value = "登录", notes = "登录", response = SystemUserVo.class)
+    @PostMapping
     public Object login(@RequestBody SystemUserVo vo) {
         SystemUserVo systemUser = systemUserService.doLogin(vo.getLoginName(), vo.getLoginPassword());
         return Result.ok(systemUser);
     }
 
+    @ApiOperation(value = "获取登录用户信息", notes = "获取登录用户信息", response = SystemUserVo.class)
     @GetMapping
     public Object getUserInfo() {
         return Result.ok(systemUserService.getUserInfo());
