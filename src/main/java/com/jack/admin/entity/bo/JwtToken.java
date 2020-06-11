cda94387d854d0c57faea7393d25e6405e17386b
lijie <crazyjie262@gmail.com>
2020-06-11 12:31:22 +0800

    update

--- src/main/java/com/jack/admin/entity/bo/JwtToken.java
+++ src/main/java/com/jack/admin/entity/bo/JwtToken.java
@@ -4,6 +4,9 @@ import org.apache.shiro.authc.AuthenticationToken;
 
 /**
  * jwt token
+ *
+ * @author crazyjack262
+ * @date 2020-06-11 12:31
  */
 public class JwtToken implements AuthenticationToken {
     private String jwt;
